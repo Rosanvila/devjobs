7404b857e417676a3b8b38ce91252e861dc82dc4
Robin Sanchez-Avila <robin.sa28@gmail.com>
2024-03-16 16:59:11 +0100

    modal click + overlay ok

--- js/dom.js
+++ js/dom.js
@@ -1,3 +1,13 @@
-const filterBtn = document.querySelector("#filter");
+const filterBtn = document.querySelector("#filter-button");
 const mobileModal = document.querySelector("#modal");
+const overlay = document.querySelector(".overlay");
 
+filterBtn.addEventListener("click", () => {
+  mobileModal.classList.add("modal-open");
+  overlay.classList.add("overlay-open");
+});
+
+overlay.addEventListener("click", () => {
+  mobileModal.classList.remove("modal-open");
+  overlay.classList.remove("overlay-open");
+});
